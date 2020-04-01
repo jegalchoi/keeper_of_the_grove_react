416e3cb8776aac0c7d824cc77fc7ab5c11526460
jegalchoi <thefreshbegal@gmail.com>
2020-04-01 14:48:34 -0700

    Setup User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,7 @@
 Rails.application.routes.draw do
 
-  # get 'plants', to: '/'
-  # get 'plants/new', to: '/'
-  # get 'plants/:id', to: '/'
-  # get 'plants/:id/edit', to: '/'
-
-
-  defaults format: :json do
-    namespace :api do
-      namespace :v1 do
-        resources :users do
-          get '/plants', to: 'plants#authorized_index'
-          get '/plants/:id', to: 'plants#authorized_show'
-          delete '/plants/:id', to: 'plants#destroy'
-          # post '/plants', to: 'plants#create'
-          patch '/plants/:id', to: 'plants#update'
-        end
-
-        # resources :plants, only: [:create, :show, :index, :update, :destroy]
-        resources :plants, only: [:index, :create, :show]
-      end
-    end
-
-    post :login, to: 'sessions#create'
-    delete :logout, to: 'sessions#destroy'
-    get :logged_in, to: 'sessions#is_logged_in?'
-    
     resources :users, only: [:create, :show, :index, :update, :destroy]
 
     root to: redirect('/')
-  
-  end
   
 end
