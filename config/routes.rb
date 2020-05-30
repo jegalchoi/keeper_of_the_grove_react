e227a9856c53b9dd39c91c561b5c72903c19081c
jegalchoi <thefreshbegal@gmail.com>
2020-05-29 19:37:17 -0700

    Adjust fallback_html

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
 
   end
 
+  get '*path', to: "application#fallback_index_html", constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
+  
   root to: redirect('/')
   
 end
