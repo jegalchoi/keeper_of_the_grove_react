d860b8b70dcedbfca1c751e2bd7ca05ff36202a0
jegalchoi <thefreshbegal@gmail.com>
2020-06-18 13:52:05 -0700

    Correct cors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_grove_guardian', domain: 'https://grove-guardian.herokuapp.com/'
+  Rails.application.config.session_store :cookie_store, key: '_grove_guardian', domain: 'grove-guardian.herokuapp.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_grove_guardian'
 end
