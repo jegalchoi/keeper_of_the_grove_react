fa2b5dd8ca18c7b406f46e4efde74dd4275fac2a
jegalchoi <thefreshbegal@gmail.com>
2020-06-18 14:13:26 -0700

    Fix session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_grove_guardian', domain: 'grove-guardian.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_grove_guardian', domain: 'groveguardian.club'
 else
   Rails.application.config.session_store :cookie_store, key: '_grove_guardian'
 end
